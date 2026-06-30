d5fca5a01de73eec62a25d85711b7ed51fd170fb
Rui-Araujo1409 <rui1409@gmail.com>
2026-06-30 11:24:51 +0100

    exercício ToDos incompleto

--- exercício ToDo/app.js
+++ exercício ToDo/app.js
@@ -1,16 +1,25 @@
 const listaToDos = ["cozinhar almoço", "levar lixo"];
 
-let comando = "novo";
+let comando = prompt("O que quer fazer?");
 
-if(comando === "novo") {
-    let toDo = "lavar louça";
-    listaToDos.push(toDo);
-    console.log(listaToDos);
-} else if(comando === "lista") {
-   for(let todo of listaToDos) {
-    console.log(`${listaToDos.indexOf(todo)}: ${todo}`);
-   }
-} else if(comando === "apagar Todo") {
-    
+while (comando !== "sair") {
+    if (comando === "novoToDo") {
+        let toDo = prompt("Insira o Novo ToDo");
+        listaToDos.push(toDo);
+        console.log(listaToDos);
+    } else if (comando === "lista") {
+        for (let todo of listaToDos) {
+            console.log("******************");
+            console.log(`${listaToDos.indexOf(todo)}: ${todo}`);
+            console.log("******************");
+        }
+    } else if (comando === "apagarToDo") {
+        let index = 2;
+        listaToDos.splice(index,1);
+        console.log(listaToDos);
+    } else if(comando === "sair") {
+        console.log("Saiu da app...")
+        break;
+    }
 }
 
