b369e9751c61c59a80b84ee20a320de0468966da
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-05 17:50:13 +0000

    crair a rota para o PATCH para alterar um comentário

--- POST_Demo/index.js
+++ POST_Demo/index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const path = require("path");
+const {v4: uuid} = require("uuid");
 const app = express();
+
 
 const comentários = [
     {
+        id: uuid(),
         username: "João",
         texto: "Este é o comentário do João."
     },
     {
+        id: uuid(),
         username: "Pedro",
         texto: "Este é o comentário do Pedro."
     },
     {
+        id: uuid(),
         username: "Maria",
         texto: "Este é o comentário da Maria."
     },
     {
+        id: uuid(),
         username: "Ana",
         texto: "Este é o comentário da Ana."
     }
@@ -40,11 +46,30 @@ app.get("/comentarios/novo", (req,res) => {
 
 app.post("/comentarios", (req,res) => {
     const {username, texto} = req.body;
-    comentários.push({username, texto});
+    comentários.push({username, texto, id: uuid()});
     //o .redirect serve para evitar reenviar o post com os dados no body
     res.redirect("/comentarios");
 })
 
+app.get("/comentarios/:id", (req,res) => {
+    const {id} = req.params;
+    //lógica para encontrar no array o obj com o id (o parseInt é necessário porque o id que
+    //vem do req.params é um string => antes de usar o UUID)
+    const comentário = comentários.find(com => com.id === id);
+    res.render("comentários/mostrar", {comentário});
+})
+
+app.patch("/comentarios/:id", (req,res) => {
+    const {id} = req.params;
+    //guardar o novo texto numa var
+    const novoComentário = req.body.texto;
+    //procurar o texto actual com o ID
+    const comentário = comentários.find(com => com.id === id);
+    //substituir o texto pelo novo
+    comentário.texto = novoComentário;
+    //depois redireccionar para a lista
+    res.redirect("/comentarios");
+})
 
 app.get("/tacos", (req,res) => {
     res.send("GET /tacos resposta");
