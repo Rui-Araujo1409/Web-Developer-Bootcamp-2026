9e555078dcad8491edddebbd8ea0460b821312b0
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-05 13:37:03 +0000

    obter lista comentários com GET

--- POST_Demo/index.js
+++ POST_Demo/index.js
@@ -1,11 +1,37 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+const comentários = [
+    {
+        username: "João",
+        texto: "Este é o comentário do João."
+    },
+    {
+        username: "Pedro",
+        texto: "Este é o comentário do Pedro."
+    },
+    {
+        username: "Maria",
+        texto: "Este é o comentário da Maria."
+    },
+    {
+        username: "Ana",
+        texto: "Este é o comentário da Ana."
+    }
+];
 
 //importante para interpretar o body que vem no POST com o application/x-www-form-urlencoded (que é o que o <form> utiliza)
 app.use(express.urlencoded({extended: true}));
 //para interpretar corpo do pedido (em JSON) com application/json
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+//implementar a rota para o GET index
+app.get("/comentarios", (req, res) => {
+    res.render("comentários/index", {comentários});
+})
 
 app.get("/tacos", (req,res) => {
     res.send("GET /tacos resposta");
