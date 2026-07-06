90111dcd6c96600189642f6165297846b1128298
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-06 15:55:39 +0000

    criar o primeiro esquema e modelo, guardar o primeiro obj no MongoDB

--- Mongoose_Basics/index.js
+++ Mongoose_Basics/index.js
@@ -11,3 +11,25 @@ const conectarMongoBD = async () => {
 }
 
 conectarMongoBD();
+
+//criar o esquema de dados para cada obj (mongoose.Schema({})), neste caso, filme (o esquema vai indicar quais os tipos de valores as propriedades irão ter)
+const filmeSchema = new mongoose.Schema({
+    título: String,
+    ano: Number,
+    pontuação: Number,
+    restrição: String,
+    emStream: Boolean
+});
+
+//depois de criar o esquema temos de criar o modelo (mongoose.model({})), que é uma classe que vai definir uma colecção no MongoDB
+//existem dois parâmetros (String, $var com Schema); o nome da var (e da String tem que ter maiúscula inicial)
+const Filme = mongoose.model("Filme", filmeSchema);
+
+//usar o modelo para criar um obj (Taxi Driver)
+const taxiDriver = new Filme({
+    título: "Taxi Driver",
+    ano: 1976,
+    pontuação: 8.2,
+    restrição: "M/16",
+    emStream: true
+});
