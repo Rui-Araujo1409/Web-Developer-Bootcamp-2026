d0aa70ac68264dbae905ee69c30738bda0d78b0f
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-06 14:36:06 +0000

    lista gatos para demo do MongoDB e mongoshell

---
+++ lista_gatos.js
@@ -0,0 +1,38 @@
+[
+    {
+        nome: "Nikita",
+        idade: 12,
+        raça: "europeu comum",
+        cor: "branco malhado",
+        peso: 5,
+        tamanho: "S",
+        personalidade: {
+            amigávelCães: false,
+            amigávelCrianças: true
+        }
+    },
+     {
+        nome: "Morticia",
+        idade: 14,
+        raça: "europeu comum",
+        cor: "preto",
+        peso: 5,
+        tamanho: "S",
+        personalidade: {
+            amigávelCães: false,
+            amigávelCrianças: false
+        }
+    },
+     {
+        nome: "Garfield",
+        idade: 22,
+        raça: "europeu comum",
+        cor: "laranja",
+        peso: 5,
+        tamanho: "M",
+        personalidade: {
+            amigávelCães: false,
+            amigávelCrianças: true
+        }
+    }
+]
