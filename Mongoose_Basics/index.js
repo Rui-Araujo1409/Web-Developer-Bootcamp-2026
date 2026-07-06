93b620578697599a5244c20dad5cc82bba7aee0c
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-06 15:29:35 +0000

    criar a BD filmes no MongoDB - no url de ligção

--- Mongoose_Basics/index.js
+++ Mongoose_Basics/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const conectarMongoBD = async () => {
     try {
-        await mongoose.connect("mongodb+srv://rui1409_db_user:5zUk18Kz4KlyTwp2@cluster0.xkdudas.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("mongodb+srv://rui1409_db_user:5zUk18Kz4KlyTwp2@cluster0.xkdudas.mongodb.net/?appName=Cluster0/BDfilmes");
         console.log("Conectado ao MongoDB");
         return mongoose;
     } catch(err) {
