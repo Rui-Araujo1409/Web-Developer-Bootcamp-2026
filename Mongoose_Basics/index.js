5e7a51017e8d7ca63d38ab940d7f65d698c495a6
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-06 15:22:59 +0000

    conectar ao MongoDB com mongoose (método async)

---
+++ Mongoose_Basics/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const conectarMongoBD = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://rui1409_db_user:5zUk18Kz4KlyTwp2@cluster0.xkdudas.mongodb.net/?appName=Cluster0");
+        console.log("Conectado ao MongoDB");
+        return mongoose;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+conectarMongoBD();
