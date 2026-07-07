f4ad0fe180bd7ea9dbe704889ce096965566a30d
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-07 15:25:26 +0000

    protecção creds com .env

--- Mongoose_Basics/produto.js
+++ Mongoose_Basics/produto.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 
 
 //Conexão ao MongoDB
 //Importante: para criar uma BD nova no MongoDB tem que se inserir o nome da BD no url da ligação, antes do ?
 const conectarMongoBD = async () => {
     try {
-        await mongoose.connect("mongodb+srv://rui1409_db_user:5zUk18Kz4KlyTwp2@cluster0.xkdudas.mongodb.net/BDfilmes?appName=Cluster0");
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Conectado ao MongoDB");
         return mongoose;
     } catch (err) {
