fc956ac4e691fde2c66e9a21445c95392ecc1cd6
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-07 19:05:36 +0000

    erros validação e actualizações com validação

--- Mongoose_Basics/produto.js
+++ Mongoose_Basics/produto.js
@@ -25,7 +25,8 @@ const produtoSchema = new mongoose.Schema({
     preço: {
         type: Number, //aqui a validação não é estrita, aceita 599 ou "599", rejeita "asdassda"
         required: true,
-        min: 20
+        min: [1, "Vender de graça? nope"] //se colocar apenas o 0 a validação é por defeito, para retornar uma mensagem de erro personalizada, usa-se um
+                                         //array [valor,mensagem] como neste caso 
     },
     emPromoção: {
         type: Boolean,
@@ -33,7 +34,7 @@ const produtoSchema = new mongoose.Schema({
     },
     categorias: [String], //aqui definimos que o valor será um array de strings
     qtd: { //vamos definir um obj nesta propriedade que vai ter os dados sobre as quantidades disponíveis
-        online: { 
+        online: {
             type: Number,
             default: 0
         },
@@ -41,6 +42,10 @@ const produtoSchema = new mongoose.Schema({
             type: Number,
             default: 0
         }
+    },
+    tamanho: {
+        type: String,
+        enum: ["S","M","L"]
     }
 });
 
@@ -50,7 +55,7 @@ const Produto = new mongoose.model("Produto", produtoSchema);
 //esta versão criar erro de validação: "Path `nome` is required."
 //const bicicleta = new Produto({preço: 599});
 
-const produtoNovo = new Produto({nome: "Capacete", preço: 21, categorias: ["segurança", "ciclismo"]});
+const produtoNovo = new Produto({ nome: "Luvas", preço: 20, categorias: ["segurança","ciclismo"], tamanho: "L" });
 //se acrescentar mais alguma propriedade (como {..., cor: "verde"}) não dá erro mas ignora a propriedade,
 //apenas cria no MongoDB nome, preço e emPromoção (com valor por defeito de false)
 
@@ -59,9 +64,22 @@ const criarProduto = async () => {
     try {
         await produtoNovo.save();
         console.log("produto criado");
-    } catch(err) {
+    } catch (err) {
         console.log(err);
-    } 
+    }
 };
 
 criarProduto();
+
+//para usar a validação ao actualizar um documento, temos que especificar que queremos usar a validação (runValidators: true)
+//na parte das opções neste caso do método .findOneAndUpdate()
+/* const alterarProduto = async () => {
+    try {
+        await Produto.findOneAndUpdate({ nome: "Cadeado" }, { preço: -1 }, { returnDocument: "after", runValidators: true });
+        console.log("produto actualizado")
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+alterarProduto(); */
