b0177f83a04be56deabfd02f196b7452dd2f581e
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-08 20:43:11 +0000

    Mongoose Middleware

--- Mongoose_Basics/pessoa.js
+++ Mongoose_Basics/pessoa.js
@@ -28,10 +28,26 @@ const pessoaSchema = new mongoose.Schema({
     }
 });
 
-//construir o Virtual, que é um método mas o que retorna uma propriedade comporta-se como uma propriedade
+//construir o Virtual, que é um método mas o que retorna uma propriedade que existe apenas do lado
+//do Mongoose, não existe no MongoDB, por exemplo, aqui em vez de ter 3 propriedades na BD (nome próprio, apelido e nome completo)
+//temos apenas duas na BD e é o que precisamos para construir o nome completo, que podemos depois usar do lado do Mongoose
 pessoaSchema.virtual("nomeCompleto").get(function () {
     console.log(`${this.nome} ${this.apelido}`);
 })
+
+//Construir o middleware, acções que são realzadas no Mongoose antes e depois de 
+//executar algo para o MongoDB
+//pre - antes
+pessoaSchema.pre("save", async function () {
+    this.nome = "Joaquim";
+    this.apelido = "Fortu de Araújo";
+    console.log("A gravar");
+});
+//post - após
+pessoaSchema.post("save", async function () {
+    console.log("Guardado");
+});
+
 
 //construir o modelo
 const Pessoa = mongoose.model("Pessoa", pessoaSchema);
