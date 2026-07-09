5ea90d60da17399a808813d4a7f2c5a1f6be1c8a
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-09 21:10:55 +0000

    mongoose + express: final editar produto

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
@@ -22,6 +22,7 @@ conectarMongoBD();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -33,13 +34,12 @@ app.get("/produtos", async (req, res) => {
     res.render("produtos/index", { produtos });
 })
 
+//rota para o form para criar um novo produto
 app.get("/produtos/novo", (req,res) => {
     res.render("produtos/novo");
 })
 
-
-
-//o pedido de criar o produto à BD implica async
+// a rota para oo pedido de criar o produto à BD implica async
 app.post("/produtos", async (req, res) => {
     const novoProduto = new Produto(req.body);
     console.log(novoProduto);
@@ -47,21 +47,28 @@ app.post("/produtos", async (req, res) => {
     res.redirect(`/produtos/${novoProduto._id}`);
 })
 
-
 //assim que o path tem :id as rotas seguintes com o mesmo
 //padrão (neste caso /produtos/) têm que usar o :id
+//rota para ver detalhes do produto
 app.get("/produtos/:id", async (req, res) => {
     const {id} = req.params;
     const produto = await Produto.findById(id);
     res.render("produtos/detalhe", {produto});
 })
 
+//rota para o form para editar
 app.get("/produtos/:id/editar", async (req,res) => {
     const {id} = req.params;
     const produto = await Produto.findById(id);
     res.render("produtos/editar", {produto});
 })
 
+//rota para inserir a alteração no MongoDB
+app.put("/produtos/:id", async (req,res) => {
+    const {id} = req.params;
+    const produto = await Produto.findByIdAndUpdate(id, req.body, {runValidators: true, returnDocument:'after'});
+    res.redirect(`/produtos/${produto._id}`);
+})
 
 
 
