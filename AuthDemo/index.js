fd7139a33c589c72bdceb054ea20e93511497bb9
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-19 17:24:36 +0000

    fim do AuthDemo e início da autenticação YelpCampo com Passport

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const sessão = require("express-session");
 const Utilizador = require("./modelos/utilizador")
 
 const conectarMongoBD = async () => {
@@ -24,30 +25,74 @@ servidor.use(express.json());
 servidor.set("views", path.join(__dirname, "views"));
 servidor.set("view engine", "ejs");
 
+servidor.use(sessão({
+    secret: "esteéosegredo",
+    resave: false, //o default é true mas a config default está obsoleta
+    saveUninitialized: false  //idem
+}));
+
+//Middleware para verificar se está logado
+const requerLogin = (req,res,next) => (!req.session.utilizador_id) ? res.redirect("/entrar") : next();
+
+
 //rotas estáticas
 
-servidor.get("/", (req,res) => {
+servidor.get("/", (req, res) => {
     res.send("Homepage!")
 })
 
-servidor.get("/registrar", (req,res) => {
+servidor.get("/registrar", (req, res) => {
     res.render("registrar");
 })
 
-servidor.get("/segredo", (req,res) =>{
-    res.send("Isto é o segredo, tem que estar logado");
-})
-
-servidor.post("/registrar", async (req,res) => {
-const {utilizador, pass} = req.body;
-const passEncriptada = await bcrypt.hash(pass, 12); 
-const novoUtilizador = new Utilizador({nome: utilizador, password: passEncriptada});
-//await novoUtilizador.save();
-console.log(novoUtilizador);
-res.redirect("/");
+servidor.get("/entrar", (req, res) => {
+    res.render("entrar");
 })
 
 
+servidor.post("/registrar", async (req, res) => {
+    const { utilizador, pass } = req.body;
+    //const passEncriptada = await bcrypt.hash(pass, 12); //podemos passar a encriptação para o modelo
+    const novoUtilizador = new Utilizador({ nome: utilizador, password: pass});
+    await novoUtilizador.save();
+    console.log(novoUtilizador);
+    //quando se resgistra o _id do Utilizador fica gravado na sessão
+    req.session.utilizador_id = novoUtilizador._id;
+    res.redirect("/");
+})
+
+servidor.post("/entrar", async (req, res) => {
+    const { utilizador, pass } = req.body;
+/*     const utilizadorVerificar = await Utilizador.findOne({ nome: utilizador });
+    const verificaçãoPass = await bcrypt.compare(pass, utilizadorVerificar.password); */
+    //depois de criar o método estático no modelo
+   const utilizadorEncontrado = await Utilizador.encontrarEValidar(utilizador, pass);
+    if (!utilizadorEncontrado) {
+        res.redirect("/entrar");
+    } else {
+        req.session.utilizador_id = utilizadorEncontrado._id;
+        res.redirect("/segredo");
+    };
+})
+
+//rota para o logout, que é apenas tornar a propriedade utilizador_id nula
+servidor.post("/sair", (req, res) => {
+    req.session.utilizador_id = null;
+    //req.session.destroy(); //este método destrói a sessão
+    res.redirect("/");
+})
+
+servidor.get("/segredo", requerLogin, (req, res) => {
+   // (req.session.utilizador_id) ? res.render("segredo") : res.redirect("/entrar"); //se autenticado aparece o formulário para o logout
+   //com o middleware requerLogin não preciso de colocar o if else anterior, só o render
+   res.render("segredo");
+})
+
+servidor.get("/maissegredo", requerLogin, (req, res) => {
+   // (req.session.utilizador_id) ? res.render("segredo") : res.redirect("/entrar"); //se autenticado aparece o formulário para o logout
+   //com o middleware requerLogin não preciso de colocar o if else anterior, só o render
+   res.render("segredo");
+})
 
 
 servidor.listen(3000, () => console.log("servir na porta 3000"));
--- AuthDemo/modelos/utilizador.js
+++ AuthDemo/modelos/utilizador.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const utilizadorSchema = new Schema({
@@ -12,6 +14,22 @@ const utilizadorSchema = new Schema({
     }
 })
 
+//o método estático (ou seja, para todas as instâncias de utilizador) tem de estar antes do modelo
+//e a fx tem de ser tradicional para usar o this, que refere o modelo do Utilizador
+
+utilizadorSchema.statics.encontrarEValidar = async function (utilizador, password) {
+    const utilizadorEncontrado = await this.findOne({nome: utilizador});
+     const validarPass = await bcrypt.compare(password, utilizadorEncontrado.password);
+     return validarPass ? utilizadorEncontrado : false; //o return é preciso!
+}
+
+//a fx para encriptar a pass vai ser com um middleware pre, NOTA: no middleware PRE já não se usa next
+//mas sim uma fx async/await
+utilizadorSchema.pre("save", async function() {
+    //método para verificar se a pass foi modificada
+    if(!this.isModified("password")) return; //será para um utilizador que crie várias contas com a mesma pass? se a pass não foi modificada, sai da fx
+    this.password = await bcrypt.hash(this.password, 12);
+    }) 
 
 const Utilizador = mongoose.model("Utilizador", utilizadorSchema);
 
