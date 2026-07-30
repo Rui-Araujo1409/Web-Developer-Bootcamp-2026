32098e6c5b421009cbb68f183ee2c0f2abce1570
Rui-Araujo1409 <rui1409@gmail.com>
2026-07-30 10:50:49 +0000

    sanitize HTML com Joi

--- YelpCampo/esquemaJoi.js
+++ YelpCampo/esquemaJoi.js
@@ -1,16 +1,38 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+//a extensão do Joi para proteger HTML
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {"string.escapeHTML": "{{#label}} must not include HTML!"},
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const limpar = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {}
+        });
+        if(limpar !== value) return helpers.error("string.escapeHTML", {value})
+          return limpar;
+      }
+    }
+  }
+})
+
+const Joi = BaseJoi.extend(extension);
 
   module.exports.parqueEsquema = Joi.object({
-        título: Joi.string().required(),
-        localização: Joi.string().required(),
+        título: Joi.string().required().escapeHTML(),
+        localização: Joi.string().required().escapeHTML(),
         preço: Joi.number().required().min(10),
        // imagem: Joi.string().required(),
-        descrição: Joi.string().required(),
+        descrição: Joi.string().required().escapeHTML(),
         apagarImagem: Joi.array()
     });
 
 module.exports.avaliaçãoEsquema = Joi.object({
-  avaliação: Joi.string().required(),
+  avaliação: Joi.string().required().escapeHTML(),
   pontuação: Joi.number().required().min(1).max(5)
 })
     
